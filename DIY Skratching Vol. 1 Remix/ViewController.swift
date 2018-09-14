93a7c99bf71c2a861b581dafca685c439fd4770e
MC Jeane <mc@wonderverse.party>
2018-09-14 11:49:26 -0400

    Initial Commit

---
+++ DIY Skratching Vol. 1 Remix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DIY Skratching Vol. 1 Remix
+//
+//  Created by Arjun Iyer on 9/14/18.
+//  Copyright © 2018 Jeane Limited Liability Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DIY Skratching Vol. 1 RemixTests/DIY_Skratching_Vol__1_RemixTests.swift
@@ -0,0 +1,36 @@
+//
+//  DIY_Skratching_Vol__1_RemixTests.swift
+//  DIY Skratching Vol. 1 RemixTests
+//
+//  Created by Arjun Iyer on 9/14/18.
+//  Copyright © 2018 Jeane Limited Liability Corporation. All rights reserved.
+//
+
+import XCTest
+@testable import DIY_Skratching_Vol__1_Remix
+
+class DIY_Skratching_Vol__1_RemixTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

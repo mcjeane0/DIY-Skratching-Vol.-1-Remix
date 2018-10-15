db1574c2d0581acee67bfdcffcdb4941a45a1777
MC Jeane <mc@wonderverse.party>
2018-10-14 20:16:06 -0400

    Build 0(27) built on  2018-10-14 at 08:16:06 PM

--- DIY Skratching Vol. 1 Remix/QBot.swift
+++ DIY Skratching Vol. 1 Remix/QBot.swift
@@ -125,7 +125,7 @@ extension QBot : FaceDelegate {
     
     func handleSwipeRight() {
         let currentSection = sections[self.faceIndexPath.section]
-        let nextPossibleRowIndex = (self.faceIndexPath.row - 1) > 0 ? self.faceIndexPath.row - 1 : 0 % videos[currentSection]!.count
+        let nextPossibleRowIndex = (self.faceIndexPath.row - 1) > 0 ? self.faceIndexPath.row - 1 : videos[currentSection]!.count
         
         faceIndexPath = IndexPath(row: nextPossibleRowIndex, section: self.faceIndexPath.section)
         loadVideoAtFaceIndexPath()

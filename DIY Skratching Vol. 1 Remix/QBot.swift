d19d40ce2981d9036db05b9052906be257a49aee
MC Jeane <mc@wonderverse.party>
2019-03-18 11:38:53 -0400

    Build 1(0) built on  2019-03-18 at 11:38:53 AM

--- DIY Skratching Vol. 1 Remix/QBot.swift
+++ DIY Skratching Vol. 1 Remix/QBot.swift
@@ -425,7 +425,7 @@ class QBot: UIResponder, UIApplicationDelegate {
     var videos : [String:[ThudRumbleVideoClip]] = [Key.Skratches.rawValue:[],
                                                    Key.EquipmentSetup.rawValue:[],
                                                    Key.Battles.rawValue:[]]
-    var playerLooper : AVPlayerLooper?
+    var player : AVPlayer?
     var queuePlayer : AVQueuePlayer?
     var playerItem : AVPlayerItem?
     var asset : AVAsset?
@@ -589,13 +589,13 @@ class QBot: UIResponder, UIApplicationDelegate {
                 break
             }
         }
-        if let playerLooper = object as? AVPlayerLooper {
-            NSLog("loopCount:\(playerLooper.loopCount)")
+        if let playerLooper = object as? AVPlayer {
+            
             DispatchQueue.main.async {
                self.queuePlayer?.rate = self.playbackRate
             }
-            face.dispatchText("\(playerLooper.loopCount)",for:5.0)
-            //
+            //face.dispatchText("\(playerLooper.loopCount)",for:5.0)
+            
         }
     }
 
@@ -667,18 +667,18 @@ class QBot: UIResponder, UIApplicationDelegate {
                     */
                     
                     if matchingVideo!.loop != nil {
-                        playerLooper = AVPlayerLooper(player: queuePlayer!, templateItem: playerItem!, timeRange: matchingVideo!.loop!)
+                        player = AVPlayer(playerItem: playerItem!)
                     }
                     else {
-                        playerLooper = AVPlayerLooper(player: queuePlayer!, templateItem:playerItem!)
+                         player = AVPlayer(playerItem: playerItem!)
                     }
                     
                     if !looped{
                         NotificationCenter.default.addObserver(self, selector: #selector(playbackEnded(says:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: playerItem)
                         
-                        playerLooper?.disableLooping()
+                        
                     }
-                    playerLooper?.addObserver(self, forKeyPath: "loopCount", options: [], context: nil)
+                    player?.addObserver(self, forKeyPath: "loopCount", options: [], context: nil)
                     face.setLayerPlayerLooper(queuePlayer!)
                     
                     completion(true)
@@ -711,18 +711,18 @@ class QBot: UIResponder, UIApplicationDelegate {
                     */
                     //
                         if matchingVideo!.loop != nil {
-                            playerLooper = AVPlayerLooper(player: queuePlayer!, templateItem: playerItem!, timeRange: matchingVideo!.loop!)
+                            player = AVPlayer(playerItem: playerItem!)
                         }
                         else {
-                            playerLooper = AVPlayerLooper(player: queuePlayer!, templateItem:playerItem!)
+                             player = AVPlayer(playerItem: playerItem!)
                         }
                     
                         if !looped{
                             NotificationCenter.default.addObserver(self, selector: #selector(playbackEnded(says:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: playerItem)
                             
-                            playerLooper?.disableLooping()
+                            
                         }
-                        playerLooper?.addObserver(self, forKeyPath: "loopCount", options: [], context: nil)
+                        player?.addObserver(self, forKeyPath: "loopCount", options: [], context: nil)
                         face.setLayerPlayerLooper(queuePlayer!)
                     
                         completion(true)
@@ -789,61 +789,19 @@ class QBot: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
     }
     
     func configureAudioSession(){
         do {
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord, mode: .spokenAudio, options: [.allowBluetoothA2DP, .allowAirPlay, .mixWithOthers,.defaultToSpeaker])
+            if #available(iOS 10.0, *) {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord, mode: .spokenAudio, options: [.allowBluetoothA2DP, .allowAirPlay, .mixWithOthers,.defaultToSpeaker])
+            } else {
+                // Fallback on earlier versions
+            }
             try AVAudioSession.sharedInstance().setActive(true, options: AVAudioSession.SetActiveOptions.notifyOthersOnDeactivation)
         }
         catch{
             NSLog("catch configure audio session")
-        }
-    }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "DIY_Skratching_Vol__1_Remix")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
         }
     }
 

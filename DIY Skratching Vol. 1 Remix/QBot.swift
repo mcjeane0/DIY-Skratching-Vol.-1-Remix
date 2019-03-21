b57682bb52d4dc4c0d9b82de4716c2d9ebd899f8
MC Jeane <mc@wonderverse.party>
2019-03-21 10:27:02 -0400

    Build 1(40) built on  2019-03-21 at 10:27:02 AM

--- DIY Skratching Vol. 1 Remix/QBot.swift
+++ DIY Skratching Vol. 1 Remix/QBot.swift
@@ -140,7 +140,7 @@ class QBot: UIResponder, UIApplicationDelegate {
         })
     }
     
-    fileprivate var babyTimes = [CMTime(value: 34961, timescale: 1000),CMTime(value: 41002, timescale: 1000),CMTime(value: 47024, timescale: 1000),CMTime(value: 53009, timescale: 1000)]
+    fileprivate var babyTimes = [CMTime(value: 34961, timescale: 1000),CMTime(value: 41020, timescale: 1000),CMTime(value: 47024, timescale: 1000),CMTime(value: 53009, timescale: 1000)]
     
     fileprivate let aMilli = CMTime(value: 1000, timescale: 1000)
     

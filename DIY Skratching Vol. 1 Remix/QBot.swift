2d0541d1df7d1c70a58c9ede42d6c36c3457db06
MC Jeane <mc@wonderverse.party>
2019-04-07 19:17:09 -0400

    Build 1.0(136) built on  2019-04-07 at 07:17:09 PM

--- DIY Skratching Vol. 1 Remix/QBot.swift
+++ DIY Skratching Vol. 1 Remix/QBot.swift
@@ -372,7 +372,7 @@ class QBot: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        face.secondTimer.start()
+        //face.secondTimer.start()
         globalPlay()
 
     }

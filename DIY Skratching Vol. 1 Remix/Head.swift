3969d8e6989d6876f7f3c3dc5a2587ae2d647a42
MC Jeane <mc@wonderverse.party>
2019-04-08 13:28:13 -0400

    Build 1.0(182) built on  2019-04-08 at 01:28:13 PM

--- DIY Skratching Vol. 1 Remix/Head.swift
+++ DIY Skratching Vol. 1 Remix/Head.swift
@@ -79,7 +79,7 @@ extension Head : FaceDelegate {
     func handleTempoButtonTapped(bpm: Float, period:Int) {
         desiredTempo = bpm
         achieveDesiredTempo()
-        let milliInterval : Int = (period/1000000)*2
+        let milliInterval : Int = (period/1000000)*4
         let interval = Repeater.Interval.milliseconds(milliInterval)
         //Repeater.Interval.milliseconds(4*period/1000) 
         
